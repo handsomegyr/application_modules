c93d1631f8b4e3b63bdbd33244700c05b77088d2
handsomegyr <handsomegyr@126.com>
2019-11-25 14:35:01 +0800

    门店增加字段

--- apps/backend/submodules/store/controllers/StoreController.php
+++ apps/backend/submodules/store/controllers/StoreController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Backend\Submodules\Store\Controllers;
 
 use App\Backend\Submodules\Store\Models\Store;
@@ -26,10 +27,10 @@ class StoreController extends \App\Backend\Controllers\FormController
             'name' => '店铺名',
             'data' => array(
                 'type' => 'string',
-                'length' => 50
+                'length' => 100
             ),
             'validation' => array(
-                'required' => 1
+                'required' => true
             ),
             'form' => array(
                 'input_type' => 'text',
@@ -39,9 +40,68 @@ class StoreController extends \App\Backend\Controllers\FormController
                 'is_show' => true
             ),
             'search' => array(
+                'is_show' => true
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+        $schemas['desc'] = array(
+            'name' => '说明',
+            'data' => array(
+                'type' => 'string',
+                'length' => 255
+            ),
+            'validation' => array(
+                'required' => true
+            ),
+            'form' => array(
+                'input_type' => 'textarea',
+                'is_show' => true
+            ),
+            'list' => array(
+                'is_show' => false
+            ),
+            'search' => array(
+                'is_show' => true
+            ),
+            'export' => array(
                 'is_show' => false
             )
         );
+
+        $schemas['logo'] = array(
+            'name' => 'LOGO图',
+            'data' => array(
+                'type' => 'string',
+                'length' => 255
+            ),
+            'validation' => array(
+                'required' => false
+            ),
+            'form' => array(
+                'input_type' => 'image',
+                'is_show' => true
+            ),
+            'list' => array(
+                'is_show' => true,
+                'render' => 'img',
+                // // 扩展设置
+                // 'extensionSettings' => function ($column, $Grid) {
+                //     //display()方法来通过传入的回调函数来处理当前列的值：
+                //     $column->display(function () {
+                //         return "<img src=" .  "/" . $this->logo . " height='50'/>";
+                //     });
+                // }
+            ),
+            'search' => array(
+                'is_show' => false
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+
         // 0关闭，1开启，2审核中
         $stateItems = array(
             array(
@@ -65,7 +125,7 @@ class StoreController extends \App\Backend\Controllers\FormController
                 'defaultValue' => 0
             ),
             'validation' => array(
-                'required' => 1
+                'required' => true
             ),
             'form' => array(
                 'input_type' => 'radio',
@@ -78,9 +138,37 @@ class StoreController extends \App\Backend\Controllers\FormController
             ),
             'search' => array(
                 'is_show' => false
+            ),
+            'export' => array(
+                'is_show' => true
             )
         );
-        
+
+        $schemas['sort'] = array(
+            'name' => '排序',
+            'data' => array(
+                'type' => 'integer',
+                'length' => 1
+            ),
+            'validation' => array(
+                'required' => true
+            ),
+            'form' => array(
+                'input_type' => 'number',
+                'is_show' => true
+            ),
+            'list' => array(
+                'is_show' => true,
+                'is_editable' => true
+            ),
+            'search' => array(
+                'is_show' => false
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+
         return $schemas;
     }
 
