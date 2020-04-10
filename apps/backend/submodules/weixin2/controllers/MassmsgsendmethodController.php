a8767c5fb11dedb0e21468868cf92da6b4df4b05
handsomegyr <handsomegyr@126.com>
2020-04-10 12:16:50 +0800

    微信组件升级

--- apps/backend/submodules/weixin2/controllers/MassmsgsendmethodController.php
+++ apps/backend/submodules/weixin2/controllers/MassmsgsendmethodController.php
@@ -3,8 +3,6 @@
 namespace App\Backend\Submodules\Weixin2\Controllers;
 
 use App\Backend\Submodules\Weixin2\Models\MassMsg\SendMethod;
-use App\Backend\Submodules\Weixin2\Models\Authorize\Authorizer;
-use App\Backend\Submodules\Weixin2\Models\Component\Component;
 
 /**
  * @title({name="群发消息发送方式"})
@@ -14,82 +12,14 @@ use App\Backend\Submodules\Weixin2\Models\Component\Component;
 class MassmsgsendmethodController extends \App\Backend\Controllers\FormController
 {
     private $modelSendMethod;
-    private $modelAuthorizer;
-    private $modelComponent;
     public function initialize()
     {
         $this->modelSendMethod = new SendMethod();
-        $this->modelAuthorizer = new Authorizer();
-        $this->modelComponent = new Component();
-
-        $this->componentItems = $this->modelComponent->getAll();
-        $this->authorizerItems = $this->modelAuthorizer->getAll();
         parent::initialize();
     }
-    protected $componentItems = null;
-    protected $authorizerItems = null;
 
     protected function getSchemas2($schemas)
-    {        $schemas['component_appid'] = array(
-            'name' => '第三方平台应用ID',
-            'data' => array(
-                'type' => 'string',
-                'length' => 255,
-                'defaultValue' => ''
-            ),
-            'validation' => array(
-                'required' => true
-            ),
-            'form' => array(
-                'input_type' => 'select',
-                'is_show' => true,
-                'items' => $this->componentItems
-            ),
-            'list' => array(
-                'is_show' => true,
-                'list_type' => '',
-                'render' => '',
-                'items' => $this->componentItems
-            ),
-            'search' => array(
-                'input_type' => 'select',
-                'is_show' => true,
-                'items' => $this->componentItems
-            ),
-            'export' => array(
-                'is_show' => true
-            )
-        );
-        $schemas['authorizer_appid'] = array(
-            'name' => '授权方应用ID',
-            'data' => array(
-                'type' => 'string',
-                'length' => 255,
-                'defaultValue' => ''
-            ),
-            'validation' => array(
-                'required' => true
-            ),
-            'form' => array(
-                'input_type' => 'select',
-                'is_show' => true,
-                'items' => $this->authorizerItems
-            ),
-            'list' => array(
-                'is_show' => true,
-                'list_type' => '',
-                'render' => '',
-                'items' => $this->authorizerItems
-            ),
-            'search' => array(
-                'input_type' => 'select',
-                'is_show' => true,
-                'items' => $this->authorizerItems
-            ),
-            'export' => array(
-                'is_show' => true
-            )
-        );
+    {
         $schemas['name'] = array(
             'name' => '群发消息发送方式名称',
             'data' => array(
@@ -117,34 +47,7 @@ class MassmsgsendmethodController extends \App\Backend\Controllers\FormControlle
                 'is_show' => true
             )
         );
-        $schemas['is_to_all'] = array(
-            'name' => '用于设定是否向全部用户发送',
-            'data' => array(
-                'type' => 'boolean',
-                'length' => 1,
-                'defaultValue' => false
-            ),
-            'validation' => array(
-                'required' => true
-            ),
-            'form' => array(
-                'input_type' => 'radio',
-                'is_show' => true,
-                'items' => $this->trueOrFalseDatas,
-                'help' => '用于设定是否向全部用户发送，值为true或false，选择true该消息群发给所有用户，选择false可根据tag_id发送给指定群组的用户',
-            ),
-            'list' => array(
-                'is_show' => true,
-                'list_type' => '1',
-                'render' => '',
-            ),
-            'search' => array(
-                'is_show' => true
-            ),
-            'export' => array(
-                'is_show' => true
-            )
-        );
+
         //群发消息发送方式 0:全部发送 1:按tag_id发送 2:按openid列表
         $sendMethodOptions = array();
         $sendMethodOptions[0] = "全部发送";
@@ -176,6 +79,34 @@ class MassmsgsendmethodController extends \App\Backend\Controllers\FormControlle
                 'input_type' => 'select',
                 'is_show' => true,
                 'items' => $sendMethodOptions,
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+        $schemas['is_to_all'] = array(
+            'name' => '用于设定是否向全部用户发送',
+            'data' => array(
+                'type' => 'boolean',
+                'length' => 1,
+                'defaultValue' => false
+            ),
+            'validation' => array(
+                'required' => true
+            ),
+            'form' => array(
+                'input_type' => 'radio',
+                'is_show' => true,
+                'items' => $this->trueOrFalseDatas,
+                'help' => '用于设定是否向全部用户发送，值为true或false，选择true该消息群发给所有用户，选择false可根据tag_id发送给指定群组的用户',
+            ),
+            'list' => array(
+                'is_show' => true,
+                'list_type' => '1',
+                'render' => '',
+            ),
+            'search' => array(
+                'is_show' => true
             ),
             'export' => array(
                 'is_show' => true
