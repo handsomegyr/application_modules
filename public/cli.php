8bf3091e3c9cb18bc26fa1231a2022740e527dc7
handsomegyr <handsomegyr@126.com>
2020-06-19 11:08:39 +0800

    移除公司cut

--- public/cli.php
+++ public/cli.php
@@ -1,4 +1,5 @@
 <?php
+
 use Phalcon\DI\FactoryDefault\CLI as CliDI;
 use Phalcon\CLI\Console as ConsoleApp;
 
@@ -7,31 +8,29 @@ try {
      * Include const
      */
     require __DIR__ . '/../config/const.php';
-    
-    defined('IS_IN_CLI_MODE') || define('IS_IN_CLI_MODE', true);
-    
-    require __DIR__ . '/../vendor/autoload.php';    
 
-    require __DIR__ . '/../library/DatabaseManager.php';
-    
+    defined('IS_IN_CLI_MODE') || define('IS_IN_CLI_MODE', true);
+
+    require __DIR__ . '/../vendor/autoload.php';
+
     require __DIR__ . '/../library/functions.php';
-    
+
     require __DIR__ . '/../library/core.php';
-    
+
     require __DIR__ . '/../library/MongoDate.php';
-    
+
     require __DIR__ . '/../library/MongoId.php';
-    
+
     require __DIR__ . '/../library/MongoRegex.php';
-    
+
     // Using the CLI factory default services container
     $di = new CliDI();
-    
+
     /**
      * Include services
      */
     require __DIR__ . '/../config/services.php';
-    
+
     /**
      * Register the autoloader and tell it to register the tasks directory
      */
@@ -43,22 +42,22 @@ try {
     /**
      * The FactoryDefault Dependency Injector automatically register the right services providing a full stack framework
      */
-    
+
     // Create a console application
     $application = new ConsoleApp();
     $application->setDI($di);
-    
+
     /**
      * Include modules
      */
     require __DIR__ . '/../config/modules.php';
-    
+
     /**
      * Process the console arguments
      */
     $arguments = array();
     $params = array();
-    
+
     foreach ($argv as $k => $arg) {
         if ($k == 1) {
             $arguments['task'] = $arg;
@@ -71,15 +70,14 @@ try {
     if (count($params) > 0) {
         $arguments['params'] = $params;
     }
-    
+
     // define global constants for the current task and action
     define('CURRENT_TASK', (isset($argv[1]) ? $argv[1] : null));
     define('CURRENT_ACTION', (isset($argv[2]) ? $argv[2] : null));
-    
+
     // handle incoming arguments
     $application->handle($arguments);
 } catch (\Phalcon\Exception $e) {
     echo $e->getMessage();
     exit(255);
 }
-
--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 use Phalcon\Mvc\Application;
 use Phalcon\DI\FactoryDefault;
 use Phalcon\DI;
@@ -11,7 +12,7 @@ error_reporting(E_ALL);
 
 ini_set("session.save_handler", "memcached"); // 是memcached不是memcache
 ini_set("session.save_path", "192.168.81.129:11211"); // 不要tcp:
-                                                      // php7升级后session_start(): Unable to clear session lock record
+// php7升级后session_start(): Unable to clear session lock record
 if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
     ini_set('session.lazy_write', 0);
 }
@@ -21,32 +22,30 @@ try {
      * Include const
      */
     require __DIR__ . '/../config/const.php';
-    
-    define('CURRENT_TIMESTAMP', time()); // 当前时间
-    
-    require __DIR__ . '/../vendor/autoload.php';    
 
-    require __DIR__ . '/../library/DatabaseManager.php';
-    
+    define('CURRENT_TIMESTAMP', time()); // 当前时间
+
+    require __DIR__ . '/../vendor/autoload.php';
+
     require __DIR__ . '/../library/functions.php';
-    
+
     require __DIR__ . '/../library/core.php';
-    
+
     require __DIR__ . '/../library/MongoDate.php';
-    
+
     require __DIR__ . '/../library/MongoId.php';
-    
+
     require __DIR__ . '/../library/MongoRegex.php';
     /**
      * The FactoryDefault Dependency Injector automatically register the right services providing a full stack framework
      */
     $di = new FactoryDefault();
-    
+
     /**
      * Include services
      */
     require __DIR__ . '/../config/services.php';
-    
+
     // Add any needed services to the DI here
     DI::reset();
     DI::setDefault($di);
@@ -54,12 +53,12 @@ try {
      * Handle the request
      */
     $application = new Application($di);
-    
+
     /**
      * Include modules
      */
     require __DIR__ . '/../config/modules.php';
-    
+
     echo $application->handle()->getContent();
 } catch (\Exception $e) {
     die($e->getMessage());
