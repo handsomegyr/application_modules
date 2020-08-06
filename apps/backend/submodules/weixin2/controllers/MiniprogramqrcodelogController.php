d729f5773222ad29795786a9624f381297f51076
handsomegyr <handsomegyr@126.com>
2020-08-06 14:17:59 +0800

    小程序扫描日志记录

---
+++ apps/backend/submodules/weixin2/controllers/MiniprogramqrcodelogController.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace App\Backend\Submodules\Weixin2\Controllers;
+
+use App\Backend\Submodules\Weixin2\Models\Miniprogram\Qrcode\Log;
+
+/**
+ * @title({name="小程序二维码扫描日志"})
+ *
+ * @name 小程序二维码扫描日志
+ */
+class MiniprogramqrcodelogController extends BaseController
+{
+    private $modelLog;
+    public function initialize()
+    {
+        $this->modelLog = new Log();
+        parent::initialize();
+    }
+
+    protected function getSchemas2($schemas)
+    {
+        $schemas['component_appid'] = array(
+            'name' => '第三方平台应用ID',
+            'data' => array(
+                'type' => 'string',
+                'length' => 255,
+                'defaultValue' => ''
+            ),
+            'validation' => array(
+                'required' => false
+            ),
+            'form' => array(
+                'input_type' => 'select',
+                'is_show' => true,
+                'items' => $this->componentItems
+            ),
+            'list' => array(
+                'is_show' => true,
+                'list_type' => '',
+                'render' => '',
+                'items' => $this->componentItems
+            ),
+            'search' => array(
+                'input_type' => 'select',
+                'is_show' => true,
+                'items' => $this->componentItems
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+        $schemas['authorizer_appid'] = array(
+            'name' => '授权方应用ID',
+            'data' => array(
+                'type' => 'string',
+                'length' => 255,
+                'defaultValue' => ''
+            ),
+            'validation' => array(
+                'required' => true
+            ),
+            'form' => array(
+                'input_type' => 'select',
+                'is_show' => true,
+                'items' => $this->authorizerItems
+            ),
+            'list' => array(
+                'is_show' => true,
+                'list_type' => '',
+                'render' => '',
+                'items' => $this->authorizerItems
+            ),
+            'search' => array(
+                'input_type' => 'select',
+                'is_show' => true,
+                'items' => $this->authorizerItems
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+
+        $schemas['weixin_user_id'] = array(
+            'name' => '小程序用户记录ID',
+            'data' => array(
+                'type' => 'string',
+                'length' => 24,
+                'defaultValue' => ''
+            ),
+            'validation' => array(
+                'required' => true
+            ),
+            'form' => array(
+                'input_type' => 'text',
+                'is_show' => true,
+                'items' => ''
+            ),
+            'list' => array(
+                'is_show' => true,
+                'list_type' => '',
+                'render' => '',
+            ),
+            'search' => array(
+                'is_show' => true
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+        $schemas['FromUserName'] = array(
+            'name' => '小程序用户OPENID',
+            'data' => array(
+                'type' => 'string',
+                'length' => 255,
+                'defaultValue' => ''
+            ),
+            'validation' => array(
+                'required' => true
+            ),
+            'form' => array(
+                'input_type' => 'text',
+                'is_show' => true,
+                'items' => ''
+            ),
+            'list' => array(
+                'is_show' => true,
+                'list_type' => '',
+                'render' => '',
+            ),
+            'search' => array(
+                'is_show' => true
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+        $schemas['scene'] = array(
+            'name' => '场景值',
+            'data' => array(
+                'type' => 'string',
+                'length' => 64,
+                'defaultValue' => ''
+            ),
+            'validation' => array(
+                'required' => true
+            ),
+            'form' => array(
+                'input_type' => 'text',
+                'is_show' => true,
+                'items' => ''
+            ),
+            'list' => array(
+                'is_show' => true,
+                'list_type' => '',
+                'render' => '',
+            ),
+            'search' => array(
+                'is_show' => true
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+        $schemas['log_time'] = array(
+            'name' => '记录时间',
+            'data' => array(
+                'type' => 'datetime',
+                'length' => 19,
+                'defaultValue' => getCurrentTime()
+            ),
+            'validation' => array(
+                'required' => false
+            ),
+            'form' => array(
+                'input_type' => 'datetimepicker',
+                'is_show' => true,
+                'items' => ''
+            ),
+            'list' => array(
+                'is_show' => true,
+                'list_type' => '',
+                'render' => '',
+            ),
+            'search' => array(
+                'is_show' => true
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+
+        return $schemas;
+    }
+
+    protected function getName()
+    {
+        return '小程序二维码扫描日志';
+    }
+
+    protected function getModel()
+    {
+        return $this->modelLog;
+    }
+}
