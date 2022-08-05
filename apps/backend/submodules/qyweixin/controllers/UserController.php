310e818f3dae39e91e3e8ec05fc74e7d709724fd
handson@intonead.com <gyr@3332502!>
2022-08-05 10:54:54 +0800

    修改

--- apps/backend/submodules/qyweixin/controllers/UserController.php
+++ apps/backend/submodules/qyweixin/controllers/UserController.php
@@ -97,7 +97,7 @@ class UserController extends BaseController
         // );
         return $tools;
     }
-    
+
     /**
      * @title({name="获取用户基本信息"})
      *
@@ -765,7 +765,7 @@ class UserController extends BaseController
             'export' => array(
                 'is_show' => true
             )
-        );        
+        );
         $schemas['direct_leader'] = array(
             'name' => '直属上级UserID',
             'data' => array(
@@ -1266,6 +1266,60 @@ class UserController extends BaseController
             ),
             'list' => array(
                 'is_show' => false,
+                'list_type' => '',
+                'render' => '',
+            ),
+            'search' => array(
+                'is_show' => true
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+        $schemas['is_exist'] = array(
+            'name' => '是否存在',
+            'data' => array(
+                'type' => 'boolean',
+                'length' => 1,
+                'defaultValue' => false
+            ),
+            'validation' => array(
+                'required' => true
+            ),
+            'form' => array(
+                'input_type' => 'radio',
+                'is_show' => true,
+                'items' => $this->trueOrFalseDatas
+            ),
+            'list' => array(
+                'is_show' => true,
+                'list_type' => '1',
+                'render' => '',
+            ),
+            'search' => array(
+                'is_show' => true
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
+        $schemas['sync_time'] = array(
+            'name' => '同步时间',
+            'data' => array(
+                'type' => 'datetime',
+                'length' => 19,
+                'defaultValue' => getCurrentTime()
+            ),
+            'validation' => array(
+                'required' => false
+            ),
+            'form' => array(
+                'input_type' => 'datetimepicker',
+                'is_show' => true,
+                'items' => ''
+            ),
+            'list' => array(
+                'is_show' => true,
                 'list_type' => '',
                 'render' => '',
             ),
