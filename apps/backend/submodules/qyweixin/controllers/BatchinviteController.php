652c9470bfaa5b60aac0b8666287ae82310a02f3
handson@intonead.com <gyr@3332502!>
2022-08-30 11:58:26 +0800

    修改

--- apps/backend/submodules/qyweixin/controllers/BatchinviteController.php
+++ apps/backend/submodules/qyweixin/controllers/BatchinviteController.php
@@ -147,6 +147,36 @@ class BatchinviteController extends BaseController
                 'is_show' => true
             )
         );
+        $schemas['agentid'] = array(
+            'name' => '应用ID',
+            'data' => array(
+                'type' => 'integer',
+                'length' => 11,
+                'defaultValue' => 0
+            ),
+            'validation' => array(
+                'required' => false
+            ),
+            'form' => array(
+                'input_type' => 'select',
+                'is_show' => true,
+                'items' => $this->agentItems
+            ),
+            'list' => array(
+                'is_show' => true,
+                'list_type' => '',
+                'render' => '',
+                'items' => $this->agentItems
+            ),
+            'search' => array(
+                'input_type' => 'select',
+                'is_show' => true,
+                'items' => $this->agentItems
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
         $schemas['user'] = array(
             'name' => '成员ID列表',
             'data' => array(
