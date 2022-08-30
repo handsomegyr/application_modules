f3e2c4cd4ba23c311ff83cfaf3ec16aea5932581
handson@intonead.com <gyr@3332502!>
2022-08-30 12:23:36 +0800

    修改

--- apps/backend/submodules/qyweixin/controllers/UseractivestatController.php
+++ apps/backend/submodules/qyweixin/controllers/UseractivestatController.php
@@ -81,6 +81,36 @@ class UseractivestatController extends BaseController
                 'is_show' => true
             )
         );
+        $schemas['agentid'] = array(
+            'name' => '应用ID',
+            'data' => array(
+                'type' => 'integer',
+                'length' => 11,
+                'defaultValue' => 0
+            ),
+            'validation' => array(
+                'required' => false
+            ),
+            'form' => array(
+                'input_type' => 'select',
+                'is_show' => true,
+                'items' => $this->agentItems
+            ),
+            'list' => array(
+                'is_show' => true,
+                'list_type' => '',
+                'render' => '',
+                'items' => $this->agentItems
+            ),
+            'search' => array(
+                'input_type' => 'select',
+                'is_show' => true,
+                'items' => $this->agentItems
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
         $schemas['day'] = array(
             'name' => '具体某天的活跃人数',
             'data' => array(
