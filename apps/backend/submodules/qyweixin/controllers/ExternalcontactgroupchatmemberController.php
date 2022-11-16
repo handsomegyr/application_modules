66724df18dc95ce72da0343dae6ade6b4f5b13e3
handson@intonead.com <gyr@3332502!>
2022-11-16 17:32:55 +0800

    修改

--- apps/backend/submodules/qyweixin/controllers/ExternalcontactgroupchatmemberController.php
+++ apps/backend/submodules/qyweixin/controllers/ExternalcontactgroupchatmemberController.php
@@ -165,6 +165,34 @@ class ExternalcontactgroupchatmemberController extends BaseController
                 'is_show' => true
             )
         );
+        $schemas['unionid'] = array(
+            'name' => '微信unionid',
+            'data' => array(
+                'type' => 'string',
+                'length' => 255,
+                'defaultValue' => ''
+            ),
+            'validation' => array(
+                'required' => false
+            ),
+            'form' => array(
+                'input_type' => 'text',
+                'is_show' => true,
+                'items' => '',
+                'help' => '外部联系人在微信开放平台的唯一身份标识（微信unionid），通过此字段企业可将外部联系人与公众号/小程序用户关联起来。仅当群成员类型是微信用户（包括企业成员未添加好友），且企业绑定了微信开发者ID有此字段（查看绑定方法）。第三方不可获取，上游企业不可获取下游企业客户的unionid字段',
+            ),
+            'list' => array(
+                'is_show' => true,
+                'list_type' => '',
+                'render' => '',
+            ),
+            'search' => array(
+                'is_show' => true
+            ),
+            'export' => array(
+                'is_show' => true
+            )
+        );
         $schemas['join_time'] = array(
             'name' => '入群时间',
             'data' => array(
